199b63e841002e286cf798a69bd831ace5e2222c
SvetlanaKanel <47015450+SvetlanaKanel@users.noreply.github.com>
2026-04-12 14:13:08 -0400

    Test Dropdown page (#8)
    * Test Dropdown page
    
    * List Data convert to dictionary Data

--- fixtures.py
+++ fixtures.py
@@ -3,6 +3,7 @@ import pytest
 from pom_arena.accordion_page import AccordionPage
 from pom_arena.home_page import HomePage
 from pom_arena.loader_page import LoaderPage
+from pom_arena.dropdown_page import DropdownPage
 
 
 @pytest.fixture
@@ -18,3 +19,8 @@ def accordion_page(page):
 @pytest.fixture
 def loader_page(page):
 	return LoaderPage(page)
+
+
+@pytest.fixture
+def dropdown_page(page):
+	return DropdownPage(page)
