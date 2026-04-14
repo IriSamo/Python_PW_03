4cd3135632f0d915669ad24a5c2016bb542f5ea1
Lara G <38481415+kgold2018@users.noreply.github.com>
2026-04-13 20:48:00 -0700

    Lg tests arena  (#11)
    * Add initial test files with lg initials
    
    * modified test button one click
    
    * add sidebar menu test using POM
    
    * add modified file for test sidebar menu using POM
    
    * rename test_lg_sidebar_menu to test_sidebar_menu

--- test_data/pages/side_menu_data.py
+++ test_data/pages/side_menu_data.py
@@ -4,8 +4,32 @@ from dataclasses import dataclass
 @dataclass(frozen=True)
 class SideMenuData:
     header: str
+    labels:tuple
 
 
 SIDE_MENU_DATA = SideMenuData(
     header="Menu",
+    labels=(
+        "Homepage",
+        "Accordion",
+        "Actions",
+        "Browser Tabs",
+        "Buttons",
+        "Calculator (JS)",
+        "Contact Us Form Test",
+        "Date Picker",
+        "DropDown Checkbox Radio",
+        "File Upload",
+        "Hidden Elements",
+        "iFrames",
+        "Loader",
+        "Loader Two",
+        "Login Portal Test",
+        "Mouse Movement",
+        "Pop Ups & Alerts",
+        "Predictive Search",
+        "Tables",
+        "Test Store",
+        "About Me",
+    )
 )
