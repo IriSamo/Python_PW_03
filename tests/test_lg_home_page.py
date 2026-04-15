9d64633f151938b6141129b42bc4fbbebf988a86
kgold2018 <kgold94@gmail.com>
2026-04-15 16:09:08 -0700

    add more tests in file

--- tests/test_lg_home_page.py
+++ tests/test_lg_home_page.py
@@ -1,9 +1,29 @@
 from playwright.sync_api import Page, expect
 
-def test_slidebar_menu_all(page : Page):
-    page.goto("https://www.automationtesting.co.uk/")
-    header1 = page.locator("h1" , has_text="Testing Arena")
+URL = "https://www.automationtesting.co.uk/"
 
+def test_homepage_h1_header(page : Page):
+    page.goto(URL)
+    header1 = page.locator("h1" , has_text="Testing Arena")
     expect(header1).to_be_visible()
 
-    print("'Testing Arena' header is visible")
+def test_homepage_url(page: Page):
+    page.goto(URL)
+    expect (page).to_have_url(URL)
+
+def test_homepage_title(page: Page):
+    page.goto(URL)
+    expect(page).to_have_title("Homepage")
+
+def test_homepage_logo(page : Page):
+    page.goto(URL)
+    logo = page.locator(".logo")
+    expect(logo).to_be_visible()
+
+def test_homepage_hamburger_opens_sidebar_menu(page: Page):
+    page.goto(URL)
+    sidebar = page.locator("#sidebar")
+    expect(sidebar).to_have_class("inactive")
+    hamburger = page.locator("a.toggle")
+    hamburger.click()
+    expect(sidebar).not_to_have_class("inactive")
