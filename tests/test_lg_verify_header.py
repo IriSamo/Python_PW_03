408833d43e9de25a8c3eb9a99fb93590cabdc664
Lara G <38481415+kgold2018@users.noreply.github.com>
2026-04-15 17:23:01 -0700

    rename test_lg_verify_header to test_lg_home_page and added more tests (#13)
    * rename test_lg_verify_header to test_lg_home_page and added more tests
    
    * add more tests in file
    
    * fixed test homepage_hamburger_opens_sidebar_menu
    
    * fixed /test_lg_home_page.py
    
    * fixed /test_lg_home_page.py
    
    * fixed /test_lg_home_page.py

--- tests/test_lg_verify_header.py
+++
@@ -1,9 +0,0 @@
-from playwright.sync_api import Page, expect
-
-def test_slidebar_menu_all(page : Page):
-    page.goto("https://www.automationtesting.co.uk/")
-    header1 = page.locator("h1" , has_text="Testing Arena")
-
-    expect(header1).to_be_visible()
-
-    print("'Testing Arena' header is visible")
